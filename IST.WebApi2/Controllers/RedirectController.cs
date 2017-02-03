928f6f790da07e88ff69ad784cf71d9a8d0f1341
Hassan <hassan@innostark.com>
2017-02-03 11:45:31 +0500

    Redierct Controller

---
+++ IST.WebApi2/Controllers/RedirectController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using RedirectResult = System.Web.Http.Results.RedirectResult;
+
+namespace IST.WebApi2.Controllers
+{
+    public class RedirectController : BaseController
+    {
+        [AllowAnonymous]
+        public IHttpActionResult Get(string userId, string email, int solutionId)
+        {
+            string url = "http://www.google.com";
+            System.Uri uri = new System.Uri(url);
+            return Redirect(uri);
+        }
+    }
+}
